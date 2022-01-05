1ba80e98cf26e4ac661e7a6b01dda8bee76aec65
Sininenblue <64654751+Sininenblue@users.noreply.github.com>
2022-01-05 20:30:02 +0800

    everything works

--- src/render.js
+++ src/render.js
@@ -1,4 +1,10 @@
 const youtubedl = require('yt-dlp-exec');
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+
+const ffmpegVersion = document.getElementById('ffmpegVersion');
+const ffmpegPath = document.getElementById('ffmpegPath');
+ffmpegVersion.textContent = `ffmpeg version ${ffmpeg.version}`;
+ffmpegPath.textContent = `ffmpeg path ${ffmpeg.path}`
 
 const urlField = document.getElementById('urlField');
 const qualityField = document.getElementById('qualityField');
@@ -11,19 +17,25 @@ const downloadButton = document.getElementById('downloadButton');
 downloadButton.onclick = download;
 
 
+
 function download() {
     isDownloading.textContent = 'currently downloading'
 
     let videoUrl = urlField.value;
-    let videoQuality = qualityField.value;
+    let videoQuality = `[height <=? ${qualityField.value}]`;
     let videoExtention = extensionField.value;
     let videoStartTime = startTimeField.value;
     let videoEndTime = endTimeField.value;
+    
+    if (qualityField.value == 'best') videoQuality = ''
+    let videoFormat = `bestvideo${videoQuality}+bestaudio[ext=m4a]/bestvideo+bestaudio/best"`
 
     youtubedl(videoUrl, {
-        o: `~/Downloads/%(title)s - by %(uploader)s.%(ext)s`,
-        format: `bestvideo[height <=? ${videoQuality}]+bestaudio[ext=m4a]/bestvideo+bestaudio/best"`,
+        o: `~/Downloads/%(title)s.%(ext)s`,
+        format: videoFormat,
+        ffmpegLocation: ffmpeg.path,
         mergeOutputFormat: videoExtention,
+        noPlaylist: true,
         externalDownloader: "ffmpeg",
         externalDownloaderArgs: `ffmpeg_i:-ss ${videoStartTime} -to ${videoEndTime}`,
     })
