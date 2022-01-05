15561c0eb81e96e22781621188b13cde3ae78bc7
Sininenblue <64654751+Sininenblue@users.noreply.github.com>
2022-01-05 14:59:15 +0800

    it works, now onto dropdowns

--- src/render.js
+++ src/render.js
@@ -1,8 +1,29 @@
+const youtubedl = require('yt-dlp-exec');
 
+const urlField = document.getElementById('urlField');
+const qualityField = document.getElementById('qualityField');
+const extensionField = document.getElementById('extensionField');
+const startTimeField = document.getElementById('startTimeField');
+const endTimeField = document.getElementById('endTimeField');
 const downloadButton = document.getElementById('downloadButton');
+downloadButton.onclick = download;
 
 
 function download() {
     console.log("pressed button")
-    
+
+    let videoUrl = urlField.value;
+    let videoQuality = qualityField.value;
+    let videoExtention = extensionField.value;
+    let videoStartTime = startTimeField.value;
+    let videoEndTime = endTimeField.value;
+
+    youtubedl(videoUrl, {
+        o: `~/Downloads/%(title)s - by %(uploader)s.%(ext)s`,
+        format: `bestvideo[height <=? ${videoQuality}]+bestaudio[ext=m4a]/bestvideo+bestaudio/best"`,
+        mergeOutputFormat: videoExtention,
+        externalDownloader: "ffmpeg",
+        externalDownloaderArgs: `ffmpeg_i:-ss ${videoStartTime} -to ${videoEndTime}`,
+    })
+        .then(output => console.log(output));
 }
