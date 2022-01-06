f6b1ffa2c2f765a85e955d5eed93871fc41a5dc4
Sininenblue <64654751+Sininenblue@users.noreply.github.com>
2022-01-06 15:34:24 +0800

    a few graphical changes and some prep for features

--- src/render.js
+++ src/render.js
@@ -2,6 +2,8 @@ const youtubedl = require('yt-dlp-exec');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 
 const urlField = document.getElementById('urlField');
+const confirmField = document.getElementById('confirmField');
+const paddingCheckBox = document.getElementById('paddingCheckBox');
 const qualityField = document.getElementById('qualityField');
 const extensionField = document.getElementById('extensionField');
 const startTimeField = document.getElementById('startTimeField');
@@ -11,14 +13,29 @@ const isDownloading = document.getElementById('isDownloading');
 const downloadButton = document.getElementById('downloadButton');
 downloadButton.onclick = download;
 
+// have a thing where if you place a complete youtube url youtube dl checks if it's valid
+// think about jquery
+function simulateDownload() {
+    youtubedl(urlField.value), {
+        getUrl: true,
+    }
+        .then(output => {
+            if (output.includes('ERROR')) {
+                console.log('ERROR');
+                confirmField.textContent = 'highlight_off';
+            } else confirmField.textContent = 'check_circle_outline';
+        })
+}
 
 
 function download() {
     isDownloading.textContent = 'currently downloading'
 
+    if (paddingCheckBox.value === 'on') console.log("check mark checked");
+    
     let videoQuality = `[height <=? ${qualityField.value}]`;
-    if (qualityField.value == 'best') videoQuality = ''
-    let videoFormat = `bestvideo${videoQuality}+bestaudio[ext=m4a]/bestvideo+bestaudio/best"`
+    if (qualityField.value === 'best') videoQuality = '';
+    let videoFormat = `bestvideo${videoQuality}+bestaudio[ext=m4a]/bestvideo+bestaudio/best"`;
 
     youtubedl(urlField.value, {
         o: `~/Downloads/%(title)s.%(ext)s`,
